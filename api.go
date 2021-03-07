dfadd2a23e1cbbefcc781eb4ad7e27ff02539735
Simon Tucker <brotherlogic@gmail.com>
2021-03-06 20:54:32 -0800

    Aha

--- api.go
+++ api.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 
 	pb "github.com/brotherlogic/hometaskqueue/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *server) AddQueue(ctx context.Context, req *pb.AddQueueRequest) (*pb.AddQueueResponse, error) {
-	return nil, fmt.Errorf("Bad request")
+	return nil, status.Errorf(codes.InvalidArgument, "Bad request")
 }
 func (s *server) GetTasks(ctx context.Context, req *pb.GetTasksRequest) (*pb.GetTasksResponse, error) {
 	return nil, fmt.Errorf("Bad request")
--- hometaskqueue_cli/cli.go
+++ hometaskqueue_cli/cli.go
@@ -2,16 +2,28 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 
 	pb "github.com/brotherlogic/hometaskqueue/proto"
 )
 
 func main() {
-	conn, err := grpc.Dial("hometaskqueue-q2ijxfqena-uw.a.run.app:443", grpc.WithInsecure())
+	systemRoots, err := x509.SystemCertPool()
+	if err != nil {
+		log.Fatal("failed to load system root CA cert pool")
+	}
+	creds := credentials.NewTLS(&tls.Config{
+		RootCAs: systemRoots,
+	})
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(creds))
+	conn, err := grpc.Dial("hometaskqueue-q2ijxfqena-uw.a.run.app:443", opts...)
 	if err != nil {
 		log.Fatalf("Dial Error: %v", err)
 	}
@@ -26,7 +38,7 @@ func main() {
 
 	res, err := client.AddQueue(ctx, &pb.AddQueueRequest{})
 	if err != nil {
-		log.Fatalf("Bad dial: %v", err)
+		log.Fatalf("Bad requests: %v", err)
 	}
 
 	log.Printf("Result: %v", res)
